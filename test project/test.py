ce38d24a17e132f0267e05803dd69c9c4b885b5d
mohamed sherif <adamsheref50@yahoo.com>
2024-09-11 17:17:29 +0300

    recursion commit

--- test project/test.py
+++ test project/test.py
@@ -1,1 +1,10 @@
-print("hello")
+def factorial(n):
+    # Base case: if n is 0 or 1, return 1
+    if n == 0 or n == 1:
+        return 1
+    # Recursive case: n * factorial of (n-1)
+    else:
+        return n * factorial(n - 1)
+
+# Test the function
+print(factorial(10)) 
